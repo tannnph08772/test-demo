dfe554b98f32d326eb35a9d736baa34b1f020ffd
dwfd <wefde>
2021-04-19 18:20:37 +0700

    update code about checkpayment

--- src/api/routes/user.route.js
+++ src/api/routes/user.route.js
@@ -1,11 +1,12 @@
 var express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
+const { createUserValidate, validate } = require('../validators/createUser.validate')
 
 router.get('/:id', userController.edit);
 router.post('/update/:id', userController.update);
 router.post('/delete/:id', userController.deleted);
-router.post('/create', userController.create);
+router.post('/create', createUserValidate(), validate, userController.create);
 router.get('/', userController.getUsers)
 
 module.exports = router;
