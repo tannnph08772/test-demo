abf748637ffa31cff5bc68d7d0f408f6d8a208a5
dwfd <wefde>
2021-04-19 14:50:08 +0700

    Revert "asdasdsad"
    This reverts commit 66836fff922884d21e0a8a1e99abb9647a755182.

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const login = (req, res, next) => {
-    console.log('aaaaaa11111');
+    console.log('aaaaaa');
     const password = req.body.password;
     console.log('aaaa')
     User.findOne({ where: { email: req.body.email } })
